46883a893de32743abe5714c23d37ba302dd90f3
Celcus <arandomguy1337@gmail.com>
2016-01-05 16:48:10 -0600

    Update E1_HelloWorld.java

--- Tutorials/E1_HelloWorld.java
+++ Tutorials/E1_HelloWorld.java
@@ -3,4 +3,18 @@ package nbexample;
 import static java.lang.System.*;
 
 public class NBExample{
+  
+  /**
+   * This is an example of a JavaDoc
+   */
+  public static void main(String[] args){
+    //This is a single-line comment
+    out.println("Hello World"); //Because we've imported System, we do not need to write "System.out.println()" anymore
+    
+    /*
+    * This is a multi-line comment.
+    * Without the two astricks (*) on the start of the multi-line comment, this will
+    * only be a multi-line comment.
+    */
+  }
 }
