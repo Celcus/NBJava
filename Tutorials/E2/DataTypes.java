efcf6e849a42516f4a1f18c6f9223eebcdfff2ba
Celcus <arandomguy1337@gmail.com>
2016-01-15 13:16:21 -0600

    Update DataTypes.java

--- Tutorials/E2/DataTypes.java
+++ Tutorials/E2/DataTypes.java
@@ -5,14 +5,23 @@
 
 public class DataTypes{
   public static void main(String[] args){
+    // PRIMITIVE VARIABLE TYPES, DESCRIPTIONS AND SIZES
     int a = 1; //"int" variables are considered "whole numbers" in math. It is a 32-bit sized variable
     double b = 1.23; //"double" variables are considered "decimals" in math. It is a 64-bit variable
     
     short c = 31342; //"short" variables are similar to "int" variables in that they are both whole numbers. It is a 16-bit variable
-    long d = 12395; //"long" variables are similar to "int" and "short". It is a 64-bit variable
-    float e = 12.4f; //"float" variables are similar to "doubles". Except it is a 32-bit variable
+    long d = 12395L; //"long" variables are similar to "int" and "short"; they have an 'L' after their number. It is a 64-bit variable
+    float e = 12.4f; //"float" variables are similar to "doubles", they have an 'f' after their number. It is a 32-bit variable. 
     
     char f = "x"; //"char" variables are letters. It is a 16-bit variable. 
-    boolean g = true; //"boolean" variables are True/False statements. It's size is not defined
+    String g = "This is a String" //Strings are sentences. 
+    boolean h = true; //"boolean" variables are True/False statements. It's size is not defined
+    
+    /*
+    * --------------------------------------------------------------------------
+    */
+    
+    // TYPE-CASTING
+    
   }
 }
