ccd4de5b383a0c6ebdc68e4b143d6059f2d16dd5
Celcus <arandomguy1337@gmail.com>
2016-01-15 11:24:11 -0600

    Update and rename Variables.java to DataTypes.java

---
+++ Tutorials/E2/DataTypes.java
@@ -0,0 +1,18 @@
+/**
+* From the "How To Java" Series on the NB.WHT forum
+* E2: Variables will focus on data types/variables, along with type casting, and Strings
+*/
+
+public class DataTypes{
+  public static void main(String[] args){
+    int a = 1; //"int" variables are considered "whole numbers" in math. It is a 32-bit sized variable
+    double b = 1.23; //"double" variables are considered "decimals" in math. It is a 64-bit variable
+    
+    short c = 31342; //"short" variables are similar to "int" variables in that they are both whole numbers. It is a 16-bit variable
+    long d = 12395; //"long" variables are similar to "int" and "short". It is a 64-bit variable
+    float e = 12.4f; //"float" variables are similar to "doubles". Except it is a 32-bit variable
+    
+    char f = "x"; //"char" variables are letters. It is a 16-bit variable. 
+    boolean g = true; //"boolean" variables are True/False statements. It's size is not defined
+  }
+}
