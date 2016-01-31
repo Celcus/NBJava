9f3f2d5977c8eed7d5f639a15c7c430ebf2d10b7
Celcus <arandomguy1337@gmail.com>
2016-01-30 23:40:21 -0600

    Update numCheck.java

--- Tutorials/E3/numCheck.java
+++ Tutorials/E3/numCheck.java
@@ -1,2 +1,10 @@
 public class numCheck{
+  public static void main(String[[]] args){
+    int num = 12;
+    if(num == 5){
+      System.out.println("Num equals 5");
+    }else{
+      System.out.println("Num does not equal 5");
+    }
+  }
 }
