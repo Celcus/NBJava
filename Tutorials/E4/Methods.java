93ab53b70672e2d385bc395aeff794b6c7a7163a
Celcus <arandomguy1337@gmail.com>
2016-02-07 18:07:08 -0600

    Update Methods.java

--- Tutorials/E4/Methods.java
+++ Tutorials/E4/Methods.java
@@ -2,4 +2,22 @@
 
 //Lets start by creating a Class for our code
 public class e4{
+  
+  //Lets create that method
+  public String containsHello(String h){
+    //We have learned about if/else statements in
+    //the E3 tutorial
+    if(h.contains("hello")){
+      return "This sentence contains 'hello'";
+    }else{
+      return "This sentence does not contain 'hello'";
+    }
+  }
+  
+  //Now lets create that main method
+  public static void main(String[] args){
+    String sentence = "Hello from the other side";
+    System.out.println(containsHello(sentence));
+  }
+  
 }
