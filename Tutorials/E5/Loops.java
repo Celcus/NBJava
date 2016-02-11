2be07f9c4efc4be48aca2883cc5f909819b53563
Celcus <arandomguy1337@gmail.com>
2016-02-11 11:34:55 -0600

    Update Loops.java

--- Tutorials/E5/Loops.java
+++ Tutorials/E5/Loops.java
@@ -1,4 +1,17 @@
 //Currently under development; Unfinished
 
 public class Loops{
+  
+  //I'm not exactly sure as to why I taught Methods before Loops... oh well :/
+  
+  //Make a main method; Hopefully from E4, 
+  //you understand how/why this method is written
+  public static void main(String [] args){
+    /**
+     * how a FOR loop works:
+     * 1. Instantiate the loop's variable (it is usually an INT or a DOUBLE)
+     * 2. Check to see if the statement is True
+     * 3. Increment/Decrement that loop varibale by X amount
+     *?
+  }
 }
