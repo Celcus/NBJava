c5de48255d0f22135d3c3b45f3b070fbf5262827
Celcus <arandomguy1337@gmail.com>
2016-03-06 15:40:07 -0600

    Update WhileLoops.java

--- Tutorials/E6/WhileLoops.java
+++ Tutorials/E6/WhileLoops.java
@@ -1,2 +1,12 @@
 public class WhileLoops{
+  public static void main(String[] args){
+    int x = 0; //we don't always need this, honestly, it depends on what is needed to be achieved.
+    while(x != 5){
+      if(x <= 5){
+        x++;
+      }else{
+        System.out.println("five");
+      }
+    }
+  }
 }
